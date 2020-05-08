373f5595323724dca7bc65b0df39da94e2231a29
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2020-05-08 20:13:18 +0530

    homepage added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { DataService } from './components/home/data.service';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    AboutComponent,
-    DashboardComponent,
-  ],
+  declarations: [AppComponent, HomeComponent, AboutComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
+      { path: '', component: HomeComponent },
       {
         path: 'name/:name',
         component: AboutComponent,
       },
-      { path: 'dashboard', component: DashboardComponent },
     ]),
   ],
   providers: [DataService],
--- src/app/components/home/data.service.ts
+++ src/app/components/home/data.service.ts
@@ -2,16 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
-import { map, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataService {
   constructor(private http: HttpClient) {}
 
   getAllData(): Observable<any> {
-    return this.http
-      .get('https://restcountries.eu/rest/v2/all')
-      .pipe(catchError(this.handleError));
+    return this.http.get('https://restcountries.eu/rest/v2/all');
   }
 
   getDetail(countryname: string): Observable<any> {
@@ -23,15 +21,5 @@ export class DataService {
         return singleCountry[0];
       })
     );
-  }
-  private handleError(error: any) {
-    console.error('server error:', error);
-    if (error.error instanceof Error) {
-      const errMessage = error.error.message;
-      return Observable.throw(errMessage);
-      // Use the following instead if using lite-server
-      // return Observable.throw(err.text() || 'backend server error');
-    }
-    return Observable.throw(error || 'Node.js server error');
   }
 }

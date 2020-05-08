b09ebd7da6e2d6a8c263734a30ad1160b48c1dd7
Pankaj Bandewar <pankaj.bandewar76@gmail.com>
2020-05-08 20:13:31 +0530

    detail page added

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -16,13 +16,9 @@ export class AboutComponent {
 
   ngOnInit() {
     let name = this.route.snapshot.params.name;
-    console.log(name);
 
     this.dataService.getDetail(name).subscribe((data: []) => {
-      console.log(typeof data);
-
       this.detail = data;
-      console.log(name, this.detail);
     });
   }
 
@@ -30,8 +26,6 @@ export class AboutComponent {
     console.log(prop);
     this.dataService.getDetail(prop).subscribe((data: []) => {
       this.detail = data;
-      console.log(prop, this.detail);
     });
-    // this.sorterService.sort(this.filteredCustomers, prop);
   }
 }
